a4c1bfae828a0c404549659f81cdb3fa8a0a512e
Cam C <ckc.cameron@gmail.com>
2020-07-08 11:04:16 -0600

    Add routes for edit, update and destroy actions for reviews

--- config/routes.rb
+++ config/routes.rb
@@ -21,5 +21,8 @@ Rails.application.routes.draw do
 
   get '/shelters/:id/reviews/new', to: 'reviews#new'
   post '/shelters/:id', to: 'reviews#create'
+  get '/shelters/:shelter_id/reviews/:review_id/edit', to: 'reviews#edit'
+  patch '/shelters/:shelter_id/reviews/:review_id', to: 'reviews#update'
+  delete '/shelters/:id/reviews/:review_id', to: 'reviews#destroy'
 
 end
