09b6532c137d929d4b77283c389c108d1b70e4ac
Cam C <ckc.cameron@gmail.com>
2020-07-09 15:08:36 -0600

    Rollback migration to remove favorite status from pets

--- db/migrate/20200709023409_add_favorite_status_to_pets.rb
+++
@@ -1,5 +0,0 @@
-class AddFavoriteStatusToPets < ActiveRecord::Migration[5.1]
-  def change
-    add_column :pets, :favorite_status, :boolean, default: false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200709023409) do
+ActiveRecord::Schema.define(version: 20200707001800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 20200709023409) do
     t.datetime "updated_at", null: false
     t.bigint "shelter_id"
     t.text "description"
-    t.boolean "favorite_status", default: false
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
 
