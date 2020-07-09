442b3a7864e69b18332fbecbb20fca10131b7418
Cam C <ckc.cameron@gmail.com>
2020-07-09 10:20:43 -0600

    Add index route for favorites

--- config/routes.rb
+++ config/routes.rb
@@ -25,4 +25,6 @@ Rails.application.routes.draw do
   patch '/shelters/:shelter_id/reviews/:review_id', to: 'reviews#update'
   delete '/shelters/:id/reviews/:review_id', to: 'reviews#destroy'
 
+  get '/favorites', to: 'favorites#index'
+
 end
