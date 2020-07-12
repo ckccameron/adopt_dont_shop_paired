81aec4a43640812beade4a772ccb923b88791181
Cam C <ckc.cameron@gmail.com>
2020-07-11 20:41:29 -0600

    Write spec for remove_pet method in favorite PORO

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -72,4 +72,27 @@ RSpec.describe Favorite do
       expect(favorite.favorite_already?(pet_1.id)).to_not eq(2)
     end
   end
+
+  describe "#remove_pet" do
+    it "takes pet out of favorites" do
+      shelter_1 = Shelter.create(name: "Denver Animal Shelter",
+                                  address: "3301 Navajo Street",
+                                  city: "Denver",
+                                  state: "CO",
+                                  zip: 80021)
+
+      pet_1 =  Pet.create(name: "Winnie",
+                          approximate_age: 3,
+                          sex: "Female",
+                          image: "https://imgur.com/r/puppies/cYqJGNo",
+                          adoption_status: "Available",
+                          shelter_id: shelter_1.id)
+
+      favorite = Favorite.new(pet_1.id => 1)
+
+      favorite.remove_pet(pet_1)
+
+      expect(favorite.count_of(pet_1.id)).to eq(0)
+    end
+  end
 end
