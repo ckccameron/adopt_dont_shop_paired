a77d3df7c284958288f4fd5b3ddbbc6b7d7085d5
Cam C <ckc.cameron@gmail.com>
2020-07-11 21:37:39 -0600

    Write spec to remove all favorites

--- spec/features/favorites/delete_favorite_spec.rb
+++ spec/features/favorites/delete_favorite_spec.rb
@@ -14,6 +14,13 @@ RSpec.describe "remove pet from favorites" do
                         image: "https://imgur.com/r/puppies/cYqJGNo",
                         adoption_status: "Available",
                         shelter_id: @shelter_1.id)
+
+    @pet_2 =  Pet.create(name: "Sir Maximus",
+                        approximate_age: 1,
+                        sex: "Male",
+                        image: "https://imgur.com/r/puppies/JGDU9mi",
+                        adoption_status: "Available",
+                        shelter_id: @shelter_1.id)
   end
 
   it "allows user to remove pet witin favorites index page" do
@@ -29,5 +36,26 @@ RSpec.describe "remove pet from favorites" do
     click_on "Remove Favorite"
     expect(page).to have_content("#{@pet_1.name} was removed from your favorites")
     expect(page).to have_content("Favorites: 0")
+  end
+
+  it "allows user to remove all pets from favorites index page" do
+    visit "/pets/#{@pet_1.id}"
+
+    within ".pets-#{@pet_1.id}" do
+      click_on "Add To Favorites"
+    end
+
+    visit "/pets/#{@pet_2.id}"
+
+    within ".pets-#{@pet_2.id}" do
+      click_on "Add To Favorites"
+    end
+
+    visit "/favorites"
+
+    click_on "Remove All Favorites"
+    expect(current_path).to eq("/favorites")
+    expect(page).to have_content("Favorites: 0")
+    expect(page).to have_content("Add Pets To Your Favorites To See Them Here")
   end
 end
