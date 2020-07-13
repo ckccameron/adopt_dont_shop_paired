ba14dc072459cfa8eba90017ca8e2d1dc0f53fd4
Cam C <ckc.cameron@gmail.com>
2020-07-13 17:10:52 -0600

    Create adoption status migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200713211516) do
+ActiveRecord::Schema.define(version: 20200713230539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20200713211516) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
   create_table "pet_applications", force: :cascade do |t|
@@ -47,6 +48,7 @@ ActiveRecord::Schema.define(version: 20200713211516) do
     t.datetime "updated_at", null: false
     t.bigint "shelter_id"
     t.text "description"
+    t.string "adoption"
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
 
