4d9dccdd41afb140f522637cbddb98f5de5dd478
Cam C <ckc.cameron@gmail.com>
2020-07-13 15:16:42 -0600

    Run migration with approval status default to false for petapplications table

--- db/migrate/20200713204816_add_adoption_status_to_pet_applications.rb
+++
@@ -1,5 +0,0 @@
-class AddAdoptionStatusToPetApplications < ActiveRecord::Migration[5.1]
-  def change
-    add_column :pet_applications, :status, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200713204816) do
+ActiveRecord::Schema.define(version: 20200713211516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 20200713204816) do
     t.bigint "application_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status"
+    t.boolean "status", default: false
     t.index ["application_id"], name: "index_pet_applications_on_application_id"
     t.index ["pet_id"], name: "index_pet_applications_on_pet_id"
   end
