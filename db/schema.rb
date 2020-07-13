69a02da98ea4556718f4fd704bcae7d9a5460cb7
Cam C <ckc.cameron@gmail.com>
2020-07-13 13:07:32 -0600

    Create migration to add adoption status column to applications table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200711224347) do
+ActiveRecord::Schema.define(version: 20200713164330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20200711224347) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
   create_table "pet_applications", force: :cascade do |t|
@@ -46,7 +47,6 @@ ActiveRecord::Schema.define(version: 20200711224347) do
     t.datetime "updated_at", null: false
     t.bigint "shelter_id"
     t.text "description"
-    t.boolean "favorite_status", default: false
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
 
