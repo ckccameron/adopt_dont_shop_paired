58f188485bcf50d68f3031b7c3dfc020d6041fce
Cam C <ckc.cameron@gmail.com>
2020-07-14 22:29:09 -0600

    Clean up files

--- spec/features/shelters/edit_from_index_spec.rb
+++ spec/features/shelters/edit_from_index_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'As a visitor' do
   describe 'When I click the EDIT button by a shelter on the index page' do
     describe 'I am redirected to a form' do
-    it 'then I can edit the details of the shelter' do
+      it 'then I can edit the details of the shelter' do
 
       shelter_1 =  Shelter.create!(name: "Aurora Animal Shelter",
                         address: "3301 Navajo Street",
@@ -45,33 +45,34 @@ RSpec.describe 'As a visitor' do
     end
   end
 
-  it 'If one or more fields are missing, the attempt to update the shelter information will return a flash message indicating which fields are missing' do
+    it 'If one or more fields are missing, the attempt to update the shelter information will return a flash message indicating which fields are missing' do
 
-    shelter_1 =  Shelter.create!(name: "Aurora Animal Shelter",
+      shelter_1 =  Shelter.create!(name: "Aurora Animal Shelter",
                       address: "3301 Navajo Street",
                       city: "Denver",
                       state: "CO",
                       zip: 80021)
 
-    visit "/shelters/"
-    expect(page).to have_content("#{shelter_1.name}")
+      visit "/shelters/"
+      expect(page).to have_content("#{shelter_1.name}")
 
-    click_on "Edit #{shelter_1.name}"
+      click_on "Edit #{shelter_1.name}"
 
-    expect(current_path).to eq("/shelters/#{shelter_1.id}/edit")
+      expect(current_path).to eq("/shelters/#{shelter_1.id}/edit")
 
-    city = 'Denver'
-    state = 'CO'
-    zip = 80206
+      city = 'Denver'
+      state = 'CO'
+      zip = 80206
 
-    fill_in :city, with: city
-    fill_in :state, with: state
-    fill_in :zip, with: zip
+      fill_in :city, with: city
+      fill_in :state, with: state
+      fill_in :zip, with: zip
 
-    click_on 'Update Shelter'
+      click_on 'Update Shelter'
 
-    expect(current_path).to eq("/shelters/#{shelter_1.id}")
-    expect(page).to have_content("Name can't be blank")
-    expect(page).to have_content("Address can't be blank")
+      expect(current_path).to eq("/shelters/#{shelter_1.id}")
+      expect(page).to have_content("Name can't be blank")
+      expect(page).to have_content("Address can't be blank")
+    end
   end
 end
