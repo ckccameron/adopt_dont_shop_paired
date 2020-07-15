2ac5f278b737e81c22cbce74a505707f90b29f37
Cam C <ckc.cameron@gmail.com>
2020-07-14 23:00:15 -0600

    Approved pet can't be deleted from pets show page

--- spec/features/pets/delete_pet_show_spec.rb
+++ spec/features/pets/delete_pet_show_spec.rb
@@ -37,4 +37,36 @@ RSpec.describe 'When I visit the pet show page' do
     expect(current_path).to eq('/pets')
     expect(page).to_not have_content('Tom')
   end
+
+  it "won't allow user to delete a pet that has approved application" do
+    shelter_1 = Shelter.create(name: "Denver Animal Shelter",
+                        address: "3301 Navajo Street",
+                        city: "Denver",
+                        state: "CO",
+                        zip: 80021)
+
+    pet_1 =  Pet.create(name: "Winnie",
+                        approximate_age: 3,
+                        sex: "Female",
+                        image: "https://imgur.com/r/puppies/cYqJGNo",
+                        adoption_status: true,
+                        shelter_id: shelter_1.id)
+
+    application_1 = Application.create(name: "John",
+                                      address: "1050 Blake St",
+                                      city: "Denver",
+                                      state: "CO",
+                                      zip: "80205",
+                                      phone_number: "3037179808",
+                                      description: "I love animals")
+
+    PetApplication.create(pet_id: pet_1.id, application_id: application_1.id)
+
+    visit "/applications/#{application_1.id}"
+
+    click_on "Approve Adoption"
+
+    visit "/pets"
+    expect(page).not_to have_link "Delete Pet"
+  end
 end
